e99488ec4f77ea6bd59a12b07c8fe2a919b39bc4
Alex Cantón <alexcantongarcia97@gmail.com>
2024-10-23 20:40:24 +0200

    Catch a potential error

--- src/app/api/books/[isbn]/route.ts
+++ src/app/api/books/[isbn]/route.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: NextRequest, { params }: { params: { isbn: string } }) {
-    const body = await req.json();
-    const { isbn } = params;
+    try {
+        const body = await req.json();
+        const { isbn } = params;
 
-    await axios.patch(`https://books-back-alpha.vercel.app/api/books/${isbn}`, body);
-    return NextResponse.json({ message: "Updated" });
+        await axios.patch(`https://books-back-alpha.vercel.app/api/books/${isbn}`, body);
+        return NextResponse.json({ message: "Updated" });
+    } catch (error: unknown) {
+        return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 })
+    }
 }
